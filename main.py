3971adbee686fbf367f5e280732b00f82a32f533
Lucas <lucas.franco.queiroz@gmail.com>
2022-04-09 21:26:51 -0300

    serial scrapying

--- main.py
+++ main.py
@@ -5,31 +5,19 @@ import datetime
 from subprocess import check_output, STDOUT
 import threading
 
+
 def now():
   return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 
 def log(message):
     logging.info(message)
     print(message)
 
-movie = "Finding Dory"
-movie_release_start = "2016-09-15"
-movie_release_end = "2016-12-08"
 
-# one year before the release (2016-09-15)
-dt_release_start = datetime.datetime.fromisoformat(movie_release_start)
-two_weeks = datetime.timedelta(14)
-start = (dt_release_start - two_weeks).strftime("%Y-%m-%d")
-# last release day (2016-12-08)
-end = movie_release_end  # not inclusive
-max_results = -1
-max_results_str = f"--max-results {max_results}" if max_results >= 0 else ""
-days = pd.date_range(start=start,end=end).astype(str).tolist()
-
-logging.basicConfig(filename=f"./movie/{movie}/log.txt", encoding='utf-8', level=logging.DEBUG, format='%(message)s')
-
-if not os.path.isdir(f"./movie/{movie}"):
-    os.mkdir(f"./movie/{movie}")
+def _create_movie_folder(movie):
+    if not os.path.isdir(f"{movies_folder}{movie}"):
+        os.mkdir(f"{movies_folder}{movie}")
 
 
 def _get_next_day(day):
@@ -39,15 +27,42 @@ def _get_next_day(day):
     return next_day
 
 
-def scrapy_daily_tweets_movie(movie, day):
-    next_day = _get_next_day(day)
+def _get_start(movie_release):
+    global days_before_release
+    start = (movie_release - days_before_release).strftime("%Y-%m-%d")
+    return start
 
-    file = f"./movie/{movie}/{day}.jl"
+
+def _get_end(movie_release):
+    global days_after_release
+    one_day = datetime.timedelta(1)
+    end = movie_release + days_after_release
+    end = end + one_day  # this is necessary because the last day is not inclusive in the search query
+    end = end.strftime("%Y-%m-%d")
+    return end
+
+
+def get_movies():
+    df = pd.read_csv("movies_search.csv")
+    info = df.apply(lambda row: (
+        row['release_id'],
+        row['release'],
+        row['search'],
+        datetime.datetime.strptime(row['release_start'], "%Y-%m-%d")
+    ), axis=1).values
+    return info
+
+
+def scrapy_tweets(search, release_id, day):
+    next_day = _get_next_day(day)
+    max_results_str = f"--max-results {max_results}" if max_results >= 0 else ""
+
+    file = f"./movies/{release_id}/{day}.jl"
     cmd = f'snscrape {max_results_str} ' \
           f'--jsonl ' \
           f'--progress ' \
           f'--since {day} ' \
-          f'twitter-search "{movie} until:{next_day}" > "{file}"'
+          f'twitter-search "{search} until:{next_day}" > "{file}"'
     log(f"{now()} | CMD: {cmd} - START")
     try:
         check_output(cmd, stderr=STDOUT, shell=True, universal_newlines=True)
@@ -56,22 +71,36 @@ def scrapy_daily_tweets_movie(movie, day):
         log(f"{now()} | CMD: {cmd} - FAIL")
         log(f"{now()} | ERROR: {cmd}\n {e.output}")
 
-began_at = datetime.datetime.now()
-log(f"{now()} | MOVIE: {movie} - START")
-threads = list()
 
-for i in range(len(days)-1):
-    t = threading.Thread(target=scrapy_daily_tweets_movie, args=(movie, days[i]))
-    threads.append(t)
-    t.start()
-    #t.join()
+movies_folder = "./movies/"
+DAYS_BEFORE_RELEASE = 14
+DAYS_AFTER_RELEASE = 14
+days_before_release = datetime.timedelta(DAYS_BEFORE_RELEASE)
+days_after_release = datetime.timedelta(DAYS_AFTER_RELEASE)
+max_results = -1
 
-for index, t in enumerate(threads):
-    t.join()
+if __name__ == "__main__":
+    for release_id, movie, search, release_date in get_movies():
+        began_at = datetime.datetime.now()
 
-finished_at = datetime.datetime.now()
-delta = finished_at - began_at
-total = str(delta)[:-7]
+        _create_movie_folder(release_id)
+        logging.basicConfig(filename=f"{movies_folder}{release_id}/log.txt", encoding='utf-8', level=logging.DEBUG, format='%(message)s')
+        log(f"{now()} | ID: {release_id} - MOVIE: {movie} - START")
 
-log(f"{now()} | MOVIE: {movie} - END")
-log(f"TOTAL: {total}")
+        # Scrapy movie
+        start = _get_start(release_date)
+        end = _get_end(release_date)  # last day is not inclusive
+        days = pd.date_range(start=start, end=end).astype(str).tolist()
+        threads = list()
+        for i in range(len(days)-1):
+            t = threading.Thread(target=scrapy_tweets, args=(search, release_id, days[i]))
+            threads.append(t)
+            t.start()
+        for index, t in enumerate(threads):
+            t.join()
+        finished_at = datetime.datetime.now()
+
+        delta = finished_at - began_at
+        total = str(delta)[:-7]
+        log(f"{now()} | MOVIE: {movie} - END")
+        log(f"TOTAL: {total}")
